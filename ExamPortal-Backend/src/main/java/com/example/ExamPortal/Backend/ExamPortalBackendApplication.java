fa59310507eb8a496896e943b70fabc79542308c
Tomal991 <intesarulhaquey4@gmail.com>
2022-01-08 22:04:35 +0600

    Registration module done front and backend

---
+++ ExamPortal-Backend/src/main/java/com/example/ExamPortal/Backend/ExamPortalBackendApplication.java
@@ -0,0 +1,45 @@
+package com.example.ExamPortal.Backend;
+
+import com.example.ExamPortal.Backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamPortalBackendApplication implements CommandLineRunner {
+    @Autowired
+    private UserService userService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamPortalBackendApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("starting");
+//        User user = new User();
+//
+//        user.setFirstName("tomal");
+//        user.setLastName("haq");
+//        user.setUsername("rashed");
+//        user.setPassword("abc");
+//        user.setEmail("abc@gmail.com");
+//        user.setProfile("default.png");
+//
+//        Role role1 = new Role();
+//        role1.setRoleId(44L);
+//        role1.setRoleName("admin");
+//
+//        Set<UserRole> userRoleSet = new HashSet<>();
+//        UserRole userRole = new UserRole();
+//        userRole.setRole(role1);
+//        userRole.setUser(user);
+//
+//        userRoleSet.add(userRole);
+//
+//        User user1=this.userService.createUser(user,userRoleSet);
+//        System.out.println(user1.getUsername());
+    }
+
+}
---
+++ ExamPortal-Frontend/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    SignupComponent,
+    LoginComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    FormsModule,
+    HttpClientModule,
+    MatSnackBarModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
